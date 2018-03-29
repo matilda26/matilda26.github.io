d8c910b18b20ac6b7daa07999bd4606c18e3dc45
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-03-29 22:25:51 +1100

    update to js and initial animations added

--- app.js
+++ app.js
@@ -26,31 +26,43 @@ var checkTurn = function(event) {
     containsWinning(playerTwo);
   }
 }
-
 function containsWinning(player) {
   winningCombos.forEach(function(combo) {
     for (var i = 0; i < 3; i++) {
-      if (player.includes(combo[i]) && player.includes(combo[i+1 ]) && player.includes(combo[i+2])) {
+      if (player.includes(combo[i]) && player.includes(combo[i+1]) && player.includes(combo[i+2])) {
         if (turn % 2 !== 0) {
           playerOneStatus.textContent = 'PLAYER ONE WINS';
           tally[0] += 1;
           playerOneScore.textContent = tally[0];
+          document.getElementById(combo[i]).classList.add('winner');
+          document.getElementById(combo[i+1]).classList.add('winner');
+          document.getElementById(combo[i+2]).classList.add('winner');
+          document.querySelector('.red-jump').classList.add('jump-animate');
+          //ADD CHARACTER WINNING ANIMATION
         } else {
           playerTwoStatus.textContent = 'PLAYER TWO WINS';
           tally[1] += 1;
           playerTwoScore.textContent = tally[1];
+          document.getElementById(combo[i]).classList.add('winner');
+          document.getElementById(combo[i+1]).classList.add('winner');
+          document.getElementById(combo[i+2]).classList.add('winner');
+          //ADD WINNING ANIMATION
         }
+      } else if (player.length > 4) {
+        playerOneStatus.textContent = "IT'S A DRAW";
+        playerTwoStatus.textContent = "IT'S A DRAW";
+        //ADD DISAPPOINTED ANIMATION
       }
     }
   })
 }
-
 var resetGame = function () {
   playerOne = [];
   playerTwo = [];
   turn = 0;
   gameBlock.forEach(function(block) {
     block.style.backgroundImage = 'url(images/empty.png)';
+    block.classList.remove('winner');
   })
   playerOneStatus.textContent = '';
   playerTwoStatus.textContent = '';
