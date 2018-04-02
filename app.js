02989c9c9331e87901a14e14c9b9235545b90810
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-04-02 21:54:39 +1000

    Final Game

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
 var gameBlock = document.querySelectorAll('.block');
-var playerOneStatus = document.querySelector('.one-status');
-var playerTwoStatus = document.querySelector('.two-status');
+var winnerStatus = document.querySelector('.status');
 var playerOneScore = document.querySelector('.pOne-score');
 var playerTwoScore = document.querySelector('.pTwo-score');
 var resetBtn = document.querySelector('.reset');
+var cheer = document.getElementById('winner-cheer');
 
 var playerOne = [];
 var playerTwo = [];
@@ -31,26 +31,26 @@ function containsWinning(player) {
     for (var i = 0; i < 3; i++) {
       if (player.includes(combo[i]) && player.includes(combo[i+1]) && player.includes(combo[i+2])) {
         if (turn % 2 !== 0) {
-          playerOneStatus.textContent = 'PLAYER ONE WINS';
+          winnerStatus.textContent = 'PLAYER ONE WINS';
           tally[0] += 1;
           playerOneScore.textContent = tally[0];
           document.getElementById(combo[i]).classList.add('winner');
           document.getElementById(combo[i+1]).classList.add('winner');
           document.getElementById(combo[i+2]).classList.add('winner');
-          document.querySelector('.red-jump').classList.add('jump-animate');
-          //ADD CHARACTER WINNING ANIMATION
+          document.querySelector('.blue-jump').classList.add('jump-animate');
+          cheer.play();
         } else {
-          playerTwoStatus.textContent = 'PLAYER TWO WINS';
+          winnerStatus.textContent = 'PLAYER TWO WINS';
           tally[1] += 1;
           playerTwoScore.textContent = tally[1];
           document.getElementById(combo[i]).classList.add('winner');
           document.getElementById(combo[i+1]).classList.add('winner');
           document.getElementById(combo[i+2]).classList.add('winner');
-          //ADD WINNING ANIMATION
+          document.querySelector('.red-jump').classList.add('jump-animate');
+          cheer.play();
         }
       } else if (player.length > 4) {
-        playerOneStatus.textContent = "IT'S A DRAW";
-        playerTwoStatus.textContent = "IT'S A DRAW";
+        winnerStatus.textContent = "IT'S A DRAW";
         //ADD DISAPPOINTED ANIMATION
       }
     }
@@ -64,8 +64,11 @@ var resetGame = function () {
     block.style.backgroundImage = 'url(images/empty.png)';
     block.classList.remove('winner');
   })
-  playerOneStatus.textContent = '';
-  playerTwoStatus.textContent = '';
+  winnerStatus.textContent = '';
+  document.querySelector('.blue-jump').classList.remove('jump-animate');
+  document.querySelector('.red-jump').classList.remove('jump-animate');
+  cheer.pause();
+  cheer.currentTime = 0;
 }
 
 gameBlock.forEach(function(block) {
